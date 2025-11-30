0d62a4851281dc240bd64cf6716b4b8512c338f8
Wallace Assis <atendimento@tattooai.online>
2025-11-30 19:57:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7677990074-c0d5a",
+  "appId": "1:499828709736:web:0b496f8ce6c747b2e11edd",
+  "apiKey": "AIzaSyC7p8i4AH1lUNxCNPZirkkjhzQR1Duf1f8",
+  "authDomain": "studio-7677990074-c0d5a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "499828709736"
+};
