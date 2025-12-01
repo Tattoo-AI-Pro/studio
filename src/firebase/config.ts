e64ce494f8027775481255bfe5c671da498a0bba
Wallace Assis <atendimento@tattooai.online>
2025-12-01 03:33:57 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:499828709736:web:0b496f8ce6c747b2e11edd",
   "apiKey": "AIzaSyC7p8i4AH1lUNxCNPZirkkjhzQR1Duf1f8",
   "authDomain": "studio-7677990074-c0d5a.firebaseapp.com",
+  "storageBucket": "studio-7677990074-c0d5a.appspot.com",
   "measurementId": "",
   "messagingSenderId": "499828709736"
 };
